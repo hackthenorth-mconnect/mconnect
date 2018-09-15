6ca248282c4de8dfdd321d6a4829e98a66b4b2a6
Emily Wang <emwangspam@gmail.com>
2018-09-15 14:49:55 -0400

    Fixed errors due to shifting files

--- mconnect/App.js
+++ mconnect/App.js
@@ -1,23 +1,12 @@
 import React from 'react';
-<<<<<<< Updated upstream
 import { StyleSheet, View } from 'react-native';
-import { MediaViewer } from './components/media-components.js';
-=======
-import AppNavigator from './AppNavigator';
->>>>>>> Stashed changes
+import AppNavigator from './components/AppNavigator';
 
 export default class App extends React.Component {
 
   render() {
-<<<<<<< Updated upstream
     return (
-      <MediaViewer />
-      // <View style={styles.container}>
-      // </View>
-=======
-    return(
-      <AppNavigator/>
->>>>>>> Stashed changes
+      <AppNavigator />
     );
   }
 }
