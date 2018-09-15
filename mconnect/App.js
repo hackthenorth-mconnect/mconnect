dd1667c9fc672a477de0cfe901cdb6122de5f670
Emily Wang <emwangspam@gmail.com>
2018-09-15 14:10:41 -0400

    Fixed app.js

--- mconnect/App.js
+++ mconnect/App.js
@@ -1,22 +1,23 @@
 import React from 'react';
+<<<<<<< Updated upstream
 import { StyleSheet, View } from 'react-native';
 import { MediaViewer } from './components/media-components.js';
+=======
+import AppNavigator from './AppNavigator';
+>>>>>>> Stashed changes
 
 export default class App extends React.Component {
+
   render() {
+<<<<<<< Updated upstream
     return (
       <MediaViewer />
       // <View style={styles.container}>
       // </View>
+=======
+    return(
+      <AppNavigator/>
+>>>>>>> Stashed changes
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
