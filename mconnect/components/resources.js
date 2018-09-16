af13f73eb289f716163a87af8de4f828b9fddddb
Charlie Friend <charles.d.friend@gmail.com>
2018-09-15 21:34:34 -0400

    Add list entry component

--- mconnect/components/resources.js
+++ mconnect/components/resources.js
@@ -1,22 +1,28 @@
 import React from 'react';
-import { FlatList, Alert, StyleSheet, Text, Button, View } from 'react-native';
+import { ScrollView, StyleSheet, Text } from 'react-native';
+
+import ResultsEntry from './ResultsEntry'
 
 export default class Question1 extends React.Component {
     static navigationOptions = { title: 'Resources', header: null };
 
   render() {
-    return ( <View style={styles.container}>
-      <Text style={styles.title}>Here are the resources we recommend for you</Text>
-    </View>
+    return ( 
+    <ScrollView contentContainerStyle={styles.contentContainer}>
+      <Text style={styles.title}>Here are some things that might help:</Text>
+      <ResultsEntry/>
+      <ResultsEntry/>
+      <ResultsEntry/>
+    </ScrollView>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#79e0e5',
+  contentContainer: {
+    justifyContent: 'flex-start',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: '#79e0e5',
+    flex: 1
+  }
 });
